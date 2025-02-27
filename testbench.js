79d4ed3aa6bc06c3967e30b849be9d9ff25abb70
Ahmet Inan <xdsopl@gmail.com>
2025-02-27 12:56:41 +0100

    added speed benchmark

--- testbench.js
+++ testbench.js
@@ -32,3 +32,36 @@ for (let i = 0; i < length; ++i)
 console.log(`max error = ${maxError}`);
 if (maxError > 1.0e-14)
 	throw new Error(`Transform error too high: ${maxError}`);
+const iterations = Math.floor(10000000.0 / (length * Math.log(length + 1)));
+for (let j = 0; j < iterations; ++j) {
+	fft.backward(buf2, buf1);
+	for (let i = 0; i < length; ++i)
+		buf2[i].scale(factor);
+	fft.forward(buf1, buf2);
+}
+const before = process.hrtime();
+for (let j = 0; j < iterations; ++j) {
+	fft.backward(buf2, buf1);
+	for (let i = 0; i < length; ++i)
+		buf2[i].scale(factor);
+	fft.forward(buf1, buf2);
+}
+const duration = process.hrtime(before);
+fft.backward(buf2, buf1);
+for (let i = 0; i < length; ++i)
+	buf2[i].scale(factor);
+for (let i = 0; i < length; ++i)
+	maxError = Math.max(maxError, buf2[i].sub(buf0[i]).abs());
+console.log(`max error after ${2 * iterations} iterations = ${maxError}`);
+function human(nano) {
+	if (nano < 1000)
+		return `${nano.toFixed(2)} ns`;
+	else if (nano < 1_000_000)
+		return `${(nano / 1000).toFixed(2)} µs`;
+	else if (nano < 1_000_000_000)
+		return `${(nano / 1_000_000).toFixed(2)} ms`;
+	else
+		return `${(nano / 1_000_000_000).toFixed(2)} s`;
+}
+const nano = duration[0] * 1e9 + duration[1];
+console.log(`duration per iteration = ${human(nano / iterations)}`);
